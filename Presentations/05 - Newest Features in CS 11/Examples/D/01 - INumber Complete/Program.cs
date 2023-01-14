8fc746c27a9b509f3b5a8fdedab95c370be6967a
Jesper Gulmann Henriksen <jesper@gulmann.dk>
2023-01-14 21:23:50 +0100

    Initial commit

---
+++ Presentations/05 - Newest Features in CS 11/Examples/D/01 - INumber Complete/Program.cs
@@ -0,0 +1,13 @@
+﻿var sequence = new[] { 42, 87, 112, 176 };
+Console.WriteLine( AddSequence(sequence));
+
+static int AddSequence( IEnumerable<int> sequence )
+{
+    int total = 0;
+	foreach (var i in sequence)
+	{
+		total += i;
+	}
+
+	return total;
+}
---
+++ Presentations/05 - Newest Features in CS 11/Examples/Data/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Wincubate.CS11.Data;
+
+public class Class1
+{
+
+}
